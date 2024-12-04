124e4d57c9ad32fdf2332b28ae57234c1bdaa86c
surajkr20 <suraj71308kumar@gmail.com>
2024-12-04 08:47:53 +0530

    basic overview of literals

--- Day1.java
+++ Day1.java
@@ -1,19 +1,19 @@
 
-public class Day1 {
-    public static void main(String a[]){
-        int num = 34;
-        short sh = 563;
-        long lg = 5999943l;
-        byte bt = 35;
+// public class Day1 {
+//     public static void main(String a[]){
+//         int num = 34;
+//         short sh = 563;
+//         long lg = 5999943l;
+//         byte bt = 35;
 
-        float fl = 34.44f;
-        double db = 345.555;
+//         float fl = 34.44f;
+//         double db = 345.555;
 
-        boolean bool = true;
+//         boolean bool = true;
 
-        char c = 'k';
+//         char c = 'k';
 
-        System.out.println(c);
-        System.out.println(bt);
-    }
-}
+//         System.out.println(c);
+//         System.out.println(bt);
+//     }
+// }
