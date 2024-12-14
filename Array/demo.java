24ac9033f376cfddbdc46a00b918250cd1e590d5
surajkr20 <suraj71308kumar@gmail.com>
2024-12-14 16:48:30 +0530

    some arr methods, string, and for each loop added

--- Array/demo.java
+++ Array/demo.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 public class demo {
     public static void main(String[] args) {
         // declaraton & initilazation (both) of array
@@ -7,12 +9,29 @@ public class demo {
 
         // System.out.println(arr[1]); // null
 
-        int[] ros; // only declaration, ros is getting defined in the stack memory
+        // int[] ros; // only declaration, ros is getting defined in the stack memory
 
-        ros = new int[5]; // initilazation: actually defined in heap memory(memory creation).
+        // ros = new int[5]; // initilazation: actually defined in heap memory(memory creation).
 
-        System.out.println(ros[1]);
+        // System.out.println(ros[1]);
 
+        // int arr[] = {12,43,23,76,45};
+
+        String name = "suraj";
+
+        for(char ch : name.toCharArray()){
+            System.out.println(ch);
+        }
+
+        // System.out.println(Arrays.toString(name.toCharArray()));
+
+        // Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        // print arr elements using for each
+        // for(int num : arr){
+        //     System.out.println(num);
+        // }
 
     }
 }
