f92d01f0f8ef3f2a451d6e63e454e7191d77d0f7
surajkr20 <suraj71308kumar@gmail.com>
2024-12-31 14:07:14 +0530

    cycle sorting algorithm

---
+++ Sorting/cyclesort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class cyclesort {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1, 5};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        cycleSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    static void cycleSort(int arr[]){
+        int i=0; 
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                // int temp = arr[i];
+                // arr[i] = arr[correct];
+                // arr[correct] = temp;
+                swap(arr, i, correct);
+            }else i++;
+        }
+        
+    }
+
+    static void swap(int arr[], int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
