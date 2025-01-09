cbfef2651d5fab8692b9fc50c680121006cd89f3
surajkr20 <suraj71308kumar@gmail.com>
2025-01-09 14:29:28 +0530

    string palindrome and string methods

---
+++ Strings/palindrome.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String value = "abcdcba";
+        String name = "suraj";
+        System.out.println(isPalindrome(name));
+    }
+
+    static boolean isPalindrome(String str){
+        if(str.length() == 0 || str == null) return true;
+        str = str.toLowerCase();
+        
+        for(int i=0; i<str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if(start != end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Strings/stringmethods.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class stringmethods {
+    public static void main(String[] args) {
+        String name = "Abhinav Suraj";
+        // for converting in array
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+}
