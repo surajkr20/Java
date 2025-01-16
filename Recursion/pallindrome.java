4d38d7605cae8e4117f1103ddfa0fe71d02e3594
surajkr20 <suraj71308kumar@gmail.com>
2025-01-16 13:04:16 +0530

    normal changes

---
+++ Recursion/pallindrome.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class pallindrome {
+    public static void main(String[] args) {
+        String name = "jaj";
+        checkPllindrome(name);
+    }
+
+    static void checkPllindrome(String name){
+        String text = name.toLowerCase();
+        
+        for(int i=0; i<name.length(); i++){
+            char 
+            System.out.println(name.toString());
+        }
+    }
+}
