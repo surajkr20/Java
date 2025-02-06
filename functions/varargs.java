5c7f03e38bfa0ec3712e77af6e9077c785a8c1ff
surajkr20 <suraj71308kumar@gmail.com>
2025-02-06 14:51:34 +0530

    cleanup files

--- functions/varargs.java
+++ functions/varargs.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class varargs {
     public static void main(String[] args) {
-        fun(12,34,3,2,4,5,5,66,76,54);
-        funstr("hello","suraj","abhinav","suraj");
+        // fun(12,34,3,2,4,5,5,66,76,54);
+        // funstr("hello","suraj","abhinav","suraj");
         multiple(12,32,'w', "Suraj");
     }
 
     static void multiple(int a, int b, char c, String ...v){
         System.out.println(Arrays.toString(v));
+        System.out.println(a + " " + b);
     }
 
     static void fun(int ...v){
