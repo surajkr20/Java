b7d52e8dd55345aa5fa7c98d655848e92092761d
surajkr20 <suraj71308kumar@gmail.com>
2025-02-18 15:48:50 +0530

    linear search uisng recursion with multiple approaches

--- Recursion02/Array/LinearSearch.java
+++ Recursion02/Array/LinearSearch.java
@@ -1,10 +1,17 @@
 package Recursion02.Array;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,5,4,6};
+        int arr[] = {1,2,3,5,4,5, 7 ,8,5};
         int target = 5;
-        System.out.println(found(arr, 0, target));
+        // System.out.println(found(arr, 0, target));
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println(findAllIndex(arr, 0, target, list));
+
     }
 
     // returning idx
@@ -18,5 +25,16 @@ public class LinearSearch {
     static boolean found01(int arr[], int idx, int target){
         if(idx == arr.length) return false;
         return arr[idx] == target || found01(arr, idx+1, target);
+    }
+
+    // finding all available target indexes
+    static ArrayList<Integer> findAllIndex(int arr[], int idx, int target, ArrayList<Integer> list){
+        if(idx == arr.length){
+            return list;
+        }
+        if(arr[idx] == target){
+            list.add(idx);
+        }
+        return findAllIndex(arr, idx+1, target, list);
     }
 }
