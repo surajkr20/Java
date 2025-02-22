d6080d9226ca17174c3e079d6076e58959dab2ea
surajkr20 <suraj71308kumar@gmail.com>
2025-02-22 15:14:44 +0530

    wrapper class, and final keyword

---
+++ OOPs/wrapperClass.java
@@ -0,0 +1,27 @@
+package oops;
+
+// A wrapper class in Java is used to convert primitive data types (int, char, etc.) into objects
+// ex- int > Integer, byte > Byte, char > Character etc.
+
+public class wrapperClass {
+    public static void main(String[] args) {
+        Integer a = 20;
+        System.out.println(a);
+
+        final int b = 34;
+        b = 35; // error
+
+        System.out.println(b);
+        
+    }
+}
+
+class A{
+    final int a = 34;
+    
+    String name;
+
+    public A(String name){
+        this.name = name;
+    }
+}
