150c976bb623044b598dffb4514f1a684d85278a
surajkr20 <suraj71308kumar@gmail.com>
2025-03-02 13:44:13 +0530

    revison previous learnings

--- Linkedlist/practise.java
+++ Linkedlist/practise.java
@@ -10,29 +10,130 @@ public class practise {
         }
     }
 
-    public static void main(String[] args) {
-        Node a = new Node(23);
-        Node b = new Node(25);
-
-        // connect to the nodes
-        a.next = b;
-
-        display01(a);
-
-        
+    public static void display01(Node head){
+        if(head == null ) return;
+        System.out.println(head.data);
+        display01(head.next);
     }
 
-    public static void display(Node head){
-        Node temp = head;
-        while( temp != null ){
-            System.out.println(temp.data + " ");
-            temp = temp.next;
+    public static class Linkedlist{
+        Node head = null;
+        Node tail = null;
+
+        int size = 0;
+
+        // insertAtEnd
+        void insertAtEnd(int val){
+            Node temp = new Node(val);
+            if(head == null){
+                head = tail = temp;
+            }else{
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        // insertAtHead
+        void insertAtHead(int val){
+            Node temp = new Node(val);
+            if(head == null){
+                // head = tail = temp;
+                insertAtEnd(val);
+            }else{
+                temp.next = head;
+                head = temp;
+            }
+            size++;
+        }
+
+        // display the all nodes
+        void display(){
+            Node temp = head;
+            while( temp!=null ){
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        // sizeOfll
+        int sizeOfll(){
+            Node temp = head;
+            int count = 0;
+            while(temp != null){
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+
+        // insertAt
+        void insertAt(int idx, int val){
+            Node t = new Node(val);
+            Node temp = head;
+
+            if(idx == sizeOfll()){
+                insertAtEnd(val);
+                return;
+            }else if(idx == 0){
+                insertAtHead(val);
+                return;
+            }else if(idx < 0 || idx > sizeOfll()){
+                System.out.println("wrong index, make sure is it valid index value");
+            }
+            for(int i=1; i<=idx-1; i++){
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;
+
+            size++;
+        }
+
+        // get value
+        int getAt(int idx){
+            Node temp = head;
+            if(idx < 0 || idx > sizeOfll()){
+                return -1;
+            }
+            for(int i=1; i<=idx; i++){
+                temp = temp.next;
+            }
+            return temp.data;
+        }
+
+        // deleteAt
+        void deleteAt(int idx){
+            Node temp = head;
+            for(int i=1; i<=idx-1; i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            size--;
         }
     }
 
-    public static void display01(Node head){
-        if( head == null ) return;
-        System.out.println(head.data);
-        display01(head.next);
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+
+        ll.insertAtEnd(45);
+        ll.insertAtEnd(55);
+
+        ll.display();
+
+        ll.insertAtHead(78);
+
+        // ll.display();
+
+        // ll.sizeOfll();
+
+        ll.insertAt(3, 33);
+        ll.display();
+
+        // System.out.println(ll.getAt(4));
+        ll.deleteAt(2);
+        ll.display();
+        System.out.println(ll.size);
     }
 }
