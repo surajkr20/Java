032ff547460122c0056cee3de9eea661391689fa
surajkr20 <suraj71308kumar@gmail.com>
2025-04-04 13:29:38 +0530

    preorder traversal

--- Trees/BinaryTree/implementation.java
+++ Trees/BinaryTree/implementation.java
@@ -1,55 +1,44 @@
 package Trees.BinaryTree;
 
 public class implementation {
-    public static class Node {
+    public static class Node{
         int val;
-        Node left;
-        Node right;
+        Node left, right;
 
-        public Node(int val) {
+        Node(int val){
             this.val = val;
+            left = right = null;
         }
     }
 
-    public static void display(Node root) {
-        if (root == null)
-            return;
-        System.out.print(root.val + " -> ");
-        if (root.left != null)
-            System.out.print(root.left.val + " ,");
-        if (root.right != null)
-            System.out.print(root.right.val + " ");
-        System.out.println();
-        display(root.left);
-        display(root.right);
+    public static void preorder(Node root){
+        if(root == null) return;
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
     }
 
     public static void main(String[] args) {
-        Node root = new Node(10);
-        Node a = new Node(8);
-        Node b = new Node(11);
-
+        Node root = new Node(5);
+        Node a = new Node(4);
+        Node b = new Node(6);
+        // connection, root to left and right
         root.left = a;
         root.right = b;
 
-        Node c = new Node(5);
-        Node d = new Node(9);
-
+        Node c = new Node(3);
+        Node d = new Node(3);
         a.left = c;
         a.right = d;
 
-        Node e = new Node(10);
-        Node f = new Node(12);
-
+        Node e = new Node(5);
+        Node f = new Node(7);
         b.left = e;
         b.right = f;
 
-        root.val = 10;
+        // preorder traversal (root->left->right)
+        preorder(root);
 
-        // System.out.println(root.val);
-        // System.out.println(root.left.val);
-        // System.out.println(root.right.val);
-
-        display(root);
+        
     }
 }
