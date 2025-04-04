e88b347d1ac9dfadfd0a9c290fcc76ddd9e14c03
surajkr20 <suraj71308kumar@gmail.com>
2025-04-04 12:26:52 +0530

    practise

--- Stacks/Practise/practise02.java
+++ Stacks/Practise/practise02.java
@@ -1,34 +1,53 @@
 package Stacks.Practise;
 
-import java.util.Stack;
+// import java.util.*;
 
 public class practise02 {
-    public static void reverse(Stack<Integer> st){
-        
+    public static class MyStack {
+        int[] arr = new int[5];
+        int top = -1;
+        // int n = arr.length-1;
+        void push(int x){
+            if(top == arr.length-1){
+                System.out.println("Stack overflow, <Error>");
+                return;
+            }
+            
+            arr[++top] = x;
+        }
+
+        int pop(){
+            if(top == -1){ // if stack is empty
+                System.out.println("Stack is Empty");
+                return -1;
+            }
+            int x = arr[arr.length-1];
+            top--;
+            return x;
+        }
+
+        int peek(){
+            return arr[arr.length - 1];
+        }
+
+        void display(){
+            for(int i=0; i<=top; i++){
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
+
     public static void main(String[] args) {
-        Stack<Integer> st = new Stack<>();
+
+        MyStack st = new MyStack();
         st.push(23);
         st.push(24);
+        st.push(25);
         st.push(26);
+        st.push(27);
 
-        // while(st.size() > 0){
-        //     System.out.println(st.peek());
-        //     st.pop();
-        // }
-        // System.out.println(st);
-
-        // int n = st.size();
-        // int[] temp = new int[n];
-
-        // for(int i=n-1; i>=0; i--){
-        //     temp[i] = st.pop();
-        // }
-
-        // for(int i=0; i<n; i++){
-        //     System.out.print(temp[i] + " ");
-        // }
-
-        reverse(st);
+        System.out.println(st.pop());
+        System.out.println(st.peek());
+        st.display();
     }
 }
