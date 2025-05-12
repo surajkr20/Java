0cffdf3c789609ddf4320653b374e2fa1ceac528
surajkr20 <suraj71308kumar@gmail.com>
2025-05-12 11:14:55 +0530

    basics of HashSet learnt

---
+++ Hashing/HashSet/BasicsOfHashset.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class BasicsOfHashset {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(34);
+        set.add(34);
+        set.add(35);
+        set.add(36);
+
+        System.out.println(set);
+        System.out.println(set.contains(35));   // return true or false if no exist or not(searching)
+        System.out.println(set.isEmpty());  // set is empty or not
+        System.out.println(set.size()); // returns no of unique elements
+
+        // set.clear();    // remove all set elements
+
+        // iterate on hashset(using enhanced loop)
+        for(int val : set){
+            System.out.println(val);
+        }
+
+        // using iterator
+        Iterator<Integer> itr = set.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next() + " ");
+        }
+    }
+}
